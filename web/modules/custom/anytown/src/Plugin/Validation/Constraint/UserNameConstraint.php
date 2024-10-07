729bb1b2124fae7367062242ab2ca514ab9c2d68
Joe Shindelar <eojthebrave@gmail.com>
2024-10-07 14:47:41 -0500

    Update plugins to use PHP attributes instead of annotations.

--- web/modules/custom/anytown/src/Plugin/Validation/Constraint/UserNameConstraint.php
+++ web/modules/custom/anytown/src/Plugin/Validation/Constraint/UserNameConstraint.php
@@ -4,25 +4,32 @@ declare(strict_types = 1);
 
 namespace Drupal\anytown\Plugin\Validation\Constraint;
 
-use Symfony\Component\Validator\Constraint;
+use Drupal\Core\Validation\Attribute\Constraint;
+use Drupal\Core\StringTranslation\TranslatableMarkup;
+use Symfony\Component\Validator\Constraints\EqualTo;
 
 /**
  * Provides a UserNameConstraint constraint.
  *
- * @Constraint(
- *   id = "AnytownUserNameConstraint",
- *   label = @Translation("User name can not be anytown", context = "Validation"),
- * )
- *
  * @see https://www.drupal.org/node/2015723.
  */
-final class UserNameConstraint extends Constraint {
+#[Constraint(
+  id: 'AnytownUserNameConstraint',
+  label: new TranslatableMarkup('User name can not be anytown', [], ['context' => 'Validation'])
+)]
+final class UserNameConstraint extends EqualTo {
 
   /**
    * Message to display for invalid username.
    *
    * @var string
    */
-  public string $message = 'Invalid user name. Can not use "anytown" as the user name.';
+  public $message = 'Invalid user name. Can not use "anytown" as the user name.';
 
+  /**
+   * The value to compare.
+   *
+   * @var string
+   */
+  public $value = 'anytown';
 }
